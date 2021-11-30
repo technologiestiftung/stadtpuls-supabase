eb8231b2452116eb3d019f84c5d2a4737a738d50
ff6347 <hash@ff6347.email>
2021-11-30 17:27:14 +0100

    STADTPULS-496: fix: sql function get_user_records_count
    Needed a table as return type and another ()
    If not it throws the error
    
    ```plain
    Query 1 ERROR: ERROR:  cannot use RETURN QUERY in a non-SETOF function
    LINE 6:     RETURN QUERY
    ```

--- supabase-docker-compose/dockerfiles/postgres/docker-entrypoint-initdb.d/50-remote-procedure-calls.sql
+++ supabase-docker-compose/dockerfiles/postgres/docker-entrypoint-initdb.d/50-remote-procedure-calls.sql
@@ -12,7 +12,11 @@ delete from auth.users
 where id = auth.uid();
 $$;
 
--- get users alphabetically (case-insensitively!)
+-- -- get users alphabetically (case-insensitively!)
+-- to remove it run
+-- drop function public.get_users_alphabetically();
+-- usage
+-- select * from public.get_users_alphabetically();
 CREATE OR REPLACE FUNCTION public.get_users_alphabetically()
   RETURNS SETOF public.user_profiles
   LANGUAGE plpgsql AS
@@ -24,15 +28,21 @@ END;
 $$;
 
 -- get records count for individual user
+
+-- to destroy it run
+-- drop function public.get_user_records_count(selected_user_id uuid);
+-- usage
+-- select * from public.get_user_records_count ('46d0c09b-3af6-4452-9392-230672c85929');
+
 CREATE OR REPLACE FUNCTION public.get_user_records_count(selected_user_id uuid)
-  RETURNS integer
+  RETURNS table(count bigint)
   LANGUAGE plpgsql AS
 $$
 BEGIN
-    RETURN QUERY
+    RETURN QUERY(
     SELECT COUNT(*) FROM public.records
     WHERE sensor_id IN (
       SELECT id FROM public.sensors WHERE user_id = selected_user_id
-    );
+    ));
 END;
 $$;
