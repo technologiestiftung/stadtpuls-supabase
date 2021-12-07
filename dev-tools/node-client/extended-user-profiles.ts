f53f1ce6a82363845dc5f599d136c9fed59f1852
Fabian Morón Zirfas <hash@ff6347.email>
2021-12-07 13:04:10 +0100

    chore: some housekeeping (#19)

--- dev-tools/node-client/extended-user-profiles.ts
+++ dev-tools/node-client/extended-user-profiles.ts
@@ -7,12 +7,68 @@ const supabase = createClient(
   process.env.SUPABASE_ANON_KEY
 );
 
+// This query string made the frontend fail
+// since postgrest had an disambiguation error
+// {
+//   "message": "More than one relationship was found for sensors and user_id",
+//   "hint": "By following the 'details' key, disambiguate the request by changing the url to /origin?select=relationship(*) or /origin?select=target!relationship(*)",
+//   "details": [
+//     {
+//       "relationship": "sensors_user_id_fkey[user_id][id]",
+//       "cardinality": "m2o",
+//       "origin": "public.sensors",
+//       "target": "public.user_profiles"
+//     },
+//     {
+//       "relationship": "sensors_user_id_fkey[user_id][id]",
+//       "cardinality": "m2o",
+//       "origin": "public.sensors",
+//       "target": "public.extended_user_profiles"
+//     }
+//   ]
+// }
+export const accountQueryString = `
+  id,
+  name,
+  display_name,
+  created_at,
+  url,
+  description,
+  sensors (
+    id,
+    name,
+    created_at,
+    connection_type,
+    external_id,
+    description,
+    location,
+    latitude,
+    longitude,
+    altitude,
+    category_id,
+    icon_id,
+    user_id,
+    records (
+      recorded_at,
+      measurements
+    ),
+    user:user_profiles!user_id (
+      name,
+      display_name
+    ),
+    category:category_id (
+      id,
+      name
+    )
+  )
+`;
+
 async function main() {
   const { data, error } = await supabase
-    .from("extended_user_profiles")
-    .select("*"); // ?
-  // console.log(error);
-  // console.log(data);
+    .from("user_profiles")
+    .select(accountQueryString); // ?
+  console.log(error);
+  console.log(data);
 }
 
 main().catch(console.error);
